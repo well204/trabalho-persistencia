8783c86a2229ba343abe0a790f72d777b93f6d96
mateusev <mateusevangelista47@gmail.com>
2025-11-10 20:16:31 -0300

    feat: adição de endpoints(maior_valor, menor_valor, média, abaixo_media, acima_media)

--- atv_01/server/main.py
+++ atv_01/server/main.py
@@ -38,6 +38,56 @@ def criar_produtos(produto: Produto):
 def listar_produtos():
     return produtos_df.to_dict(orient = "records")
 
+
+
+@app.get("/produtos/max")
+def max_produto():
+    global produtos_df
+    idx_max = produtos_df["preco"].idxmax()
+    produto_max = produtos_df.loc[idx_max]
+    # if produto_max.empty:
+    #     raise HTTPException(404, detail="Erro ao buscar o produto de maior valor")
+    return {
+        "id": int(produto_max["id"]),
+        "nome": produto_max["nome"],
+        "preco": float(produto_max["preco"])
+    }
+
+@app.get("/produtos/min")
+def min_produto():
+    global produtos_df
+    idx_min = produtos_df["preco"].idxmin()
+    produto_min = produtos_df.loc[idx_min]
+    if produto_min.empty:
+        raise HTTPException(404, detail="Erro ao buscar o produto de menor valor")
+    return {
+        "id": int(produto_min["id"]),
+        "nome": produto_min["nome"],
+        "preco": float(produto_min["preco"])
+    }
+
+@app.get("/produtos/mean")
+def mean():
+    global produtos_df
+    mean = produtos_df["preco"].mean()
+    return {
+        "Mensagem": f"média dos produtos {mean}"
+    }
+    
+@app.get("/produtos/acima_media")
+def over_mean():
+    global produtos_df
+    mean = produtos_df["preco"].mean()
+    over = produtos_df[produtos_df["preco"] >= mean]
+    return over.to_dict(orient = "records")
+
+@app.get("/produtos/abaixo_media")
+def under_mean():
+    global produtos_df
+    mean = produtos_df["preco"].mean()
+    under = produtos_df[produtos_df["preco"] < mean]
+    return under.to_dict(orient = "records")
+
 @app.get("/produtos/{id}")
 def obter_produto(id: int):
     global produtos_df
